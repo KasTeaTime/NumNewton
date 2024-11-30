a6da0ed5c2f17fe4d626fed29482caedf7ee7fb4
KasTeaTime <szawkalo@interia.pl>
2024-11-30 15:08:53 +0100

    2

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hi")
+import numpy as np
+from numpy.polynomial import Polynomial
+
+def reading(filename):
+    with open(filename) as file:
+        for index, line in enumerate(file):
+            if index == 0:
+                degree=int(line[:-1])
+            elif index==1:
+                    coefficients = line.split(",")
+                    print(coefficients)
+    return(degree, coefficients)
+    
+                    
+
+
+                    
+
+ep=0.001
+x0=5
+           
+degree, coefficients = reading("data.txt")
+
+
+coefficients = complex(coefficients)
+print(coefficients)
