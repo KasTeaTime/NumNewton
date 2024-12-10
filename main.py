0332c637d022114b3d499de7e82cfb2a40b342dd
KasTeaTime <szawkalo@interia.pl>
2024-12-10 01:12:28 +0100

    10

--- main.py
+++ main.py
@@ -21,14 +21,12 @@ def reading(filename):
 
 def contraction(polynomial, derivative, N_root):
     L =0.1  # Stała (musi być mniejsza od 1)
-    #N_root = root - polynomial(root) / derivative(root)
     root = N_root+ polynomial(N_root) / derivative(N_root)
     
     r = abs(N_root - root) / (1 - L)
-    print(N_root - root)
 
-    #if abs(r - N_root) < 10e-6:
-    #    return False
+    if abs(r - N_root) < 10e-6: # punkt początkowy jest daleko od rzeczywistego miejsca zerowego
+        return False
 
     # Sprawdzenie kontrakcji w kuli o promieniu r
     for theta in np.linspace(0, 2 * np.pi, 100):  # Kąt
@@ -52,30 +50,35 @@ def newton(coeffs, guess_num = 100, max_iter=100, tolerance=10e-6):
     derivative = polynomial.deriv()
     
     # Losowy wybór początkowych punktów startowych w płaszczyźnie zespolonej
-    guesses = [complex(np.random.uniform(-50, 50), np.random.uniform(-50, 50)) for _ in range(guess_num)]   #szukamy na przedziale -50 50
+    guesses = [complex(np.random.uniform(-10, 10), np.random.uniform(-10, 10)) for _ in range(guess_num)]   #szukamy na przedziale -50 50
 
-    for guess in guesses:
+    for iter_guess, guess in enumerate(guesses):
+        print(f"Guess number: {iter_guess}")
         #if len(roots) < degree:
         if True:    
             x = guess
+
+            # if sum([x.real, x.imag]) < 0.1:
+            #     print("Ziemniak")
+
             for i in range(max_iter):
+                # if i % 10 == 0:
+                #     print(f"Iteration: {i}")
                 f_x = polynomial(x)
                 f_prime_x = derivative(x)
 
                 if abs(f_prime_x) < 1e-14: #czy moduł z liczby nie jest zbyt blisko 0 
                     print("Terrible precision ")
                 
-                x_next = x - f_x / f_prime_x
-                
-                # Sprawdzenie zbieżności
+                x = x - f_x / f_prime_x
 
-                if abs(x_next - x) < tolerance:
-                    if contraction(polynomial, derivative, x_next):
-                        x = x_next
-                        break
-            # Dodaj pierwiastek do listy, jeśli nie jest zbyt blisko istniejących
-            if not any(abs(x - r) < tolerance for r in roots): 
-                roots.append(x) 
+            if contraction(polynomial, derivative, x):
+                # Dodaj pierwiastek do listy, jeśli nie jest zbyt blisko istniejących
+                if not any(abs(x - r) < tolerance for r in roots): 
+                    roots.append(x)
+                    print(f"Found root {x}")
+
+ 
     return roots                     
 
     
@@ -85,7 +88,7 @@ def newton(coeffs, guess_num = 100, max_iter=100, tolerance=10e-6):
 #     exit()
 # data = sys.argv[1]
 
-data = 'data.txt'
+data = 'data2.txt'
 
 def P(x):
     return x**2 - 2
@@ -100,19 +103,13 @@ print(contraction(P, P_prime, root))  # Oczekiwany wynik: False
 
 
 
-
-
-
-
-
-
 degree, coefficients = reading(data)
 coefficients = [c.replace('i', 'j') for c in coefficients] 
 coefficients = [complex(c) for c in coefficients]
 
-#roots = newton(coefficients)
-# np.set_printoptions(precision=2, floatmode='fixed', linewidth=100, suppress=True) #Formatowanie wypisywania w konsoli
-# print("miejsca zerowe wielomianu")
-# print(np.round(roots,2)) 
-# if degree - len(roots) != 0:
-#     print( "podejrzenie", (degree - len(roots)), " pierwiastkow wielokrotnych")
+roots = newton(coefficients)
+np.set_printoptions(precision=2, floatmode='fixed', linewidth=100, suppress=True) #Formatowanie wypisywania w konsoli
+print("miejsca zerowe wielomianu")
+print(np.round(roots,2)) 
+if degree - len(roots) != 0:
+    print( "podejrzenie", (degree - len(roots)), " pierwiastkow wielokrotnych")
