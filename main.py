9ae0f40b272f501ec00fce95b8776234297d776b
KasTeaTime <szawkalo@interia.pl>
2024-12-10 01:37:49 +0100

    12

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # W pierwszej linijce znajduje się stopień wielomianu i ewentualny komentarz zaś w kolejnych współczynniki. 
 #Dane powinny być odzielone enterami
 #Jednostka urojona powinna być zapisana jako i lub j. 
+#Szukamy miejsca zerowego na przedziale -50 50
 
 import numpy as np
 import sys
@@ -25,7 +26,7 @@ def contraction(polynomial, derivative, N_root):
     
     r = abs(N_root - root) / (1 - L)
 
-    if abs(root - N_root) < 10e-6: # punkt początkowy jest daleko od rzeczywistego miejsca zerowego
+    if abs(root - N_root) < 10e-6: # Punkt początkowy jest daleko od rzeczywistego miejsca zerowego
         # Sprawdzenie kontrakcji w kuli o promieniu r
         for theta in np.linspace(0, 2 * np.pi, 100):  # Kąt
             for rad in np.linspace(0, r, 100):       # Promień
@@ -44,64 +45,39 @@ def contraction(polynomial, derivative, N_root):
         return False
      
 
-def newton(coeffs, guess_num = 10, max_iter=100, tolerance=10e-6): 
+def newton(coeffs, guess_num = 1000, max_iter=100, tolerance=10e-6): 
     degree = len(coeffs) - 1
     roots = []
     polynomial = Polynomial(coeffs)
     derivative = polynomial.deriv()
     
     # Losowy wybór początkowych punktów startowych w płaszczyźnie zespolonej
-    guesses = [complex(np.random.uniform(-10, 10), np.random.uniform(-10, 10)) for _ in range(guess_num)]   #szukamy na przedziale -50 50
+    guesses = [complex(np.random.uniform(-50, 50), np.random.uniform(-50, 50)) for _ in range(guess_num)]   #szukamy na przedziale -50 50
 
     for iter_guess, guess in enumerate(guesses):
-        print(f"Guess number: {iter_guess}")
-        #if len(roots) < degree:
-        if True:    
+        if len(roots) < degree:    
             x = guess
 
-            # if sum([x.real, x.imag]) < 0.1:
-            #     print("Ziemniak")
-
             for i in range(max_iter):
-                # if i % 10 == 0:
-                #     print(f"Iteration: {i}")
                 f_x = polynomial(x)
                 f_prime_x = derivative(x)
 
-                if abs(f_prime_x) < 1e-14: #czy moduł z liczby nie jest zbyt blisko 0 
+                if abs(f_prime_x) < 1e-14: #Czy moduł z liczby nie jest zbyt blisko 0 
                     print("Terrible precision ")
-                
+
                 x = x - f_x / f_prime_x
 
             if contraction(polynomial, derivative, x):
-                # Dodaj pierwiastek do listy, jeśli nie jest zbyt blisko istniejących
-                if not any(abs(x - r) < tolerance for r in roots): 
+                if not any(abs(x - r) < tolerance for r in roots): # Dodaj pierwiastek do listy, jeśli nie jest zbyt blisko istniejących
                     roots.append(x)
-                    print(f"Found root {x}")
-
- 
     return roots                     
 
     
 #wczytanie danych   
-# if len(sys.argv) < 2:
-#     print("Podaj nazwe pliku")
-#     exit()
-# data = sys.argv[1]
-
-data = 'data.txt'
-
-def P(x):
-    return x**2 - 2
-
-def P_prime(x):
-    return 2 * x
-
-root = 1.4  # Blisko sqrt(2)
-print(contraction(P, P_prime, root))  # Oczekiwany wynik: True
-root = 10  # Daleko od sqrt(2)
-print(contraction(P, P_prime, root))  # Oczekiwany wynik: False
-
+if len(sys.argv) < 2:
+    print("Podaj nazwe pliku")
+    exit()
+data = sys.argv[1]
 
 
 degree, coefficients = reading(data)
